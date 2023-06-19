c7bab7cc89b8ba6fcad6feee03726b01b4893421
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-06-20 00:08:58 +0300

    сохранили изменения

---
+++ task19_dz_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+﻿Console.WriteLine("Введите пятизначное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+
+ if (amount < 5 | amount > 5)
+ {  
+    Console.WriteLine("Это не пятизначное число");
+ }
+ else 
+ {
+    Console.WriteLine(number); // на количество цифр проверяет, осталось сравнение
+ }
+
---
+++ task23_dz_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+
+// while (i <= number)
+// {
+//     System.Console.WriteLine(Math.Pow(i , 2) + " ");
+//     i++;
+// }
+
+for (int i = 1; i <= number; i++) // счетчик объявляем сразу, отдельно переменную не создаем
+{
+    System.Console.WriteLine(Math.Pow(i , 3) + " ");
+}
